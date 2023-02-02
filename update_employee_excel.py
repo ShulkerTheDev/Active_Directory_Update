bd5935c82184ee5da29a10ad2dfc615f3425f419
Devaughn Thompson <devaughnt@nib-bahamas.com>
2023-02-02 13:50:18 -0500

    Recent changes

---
+++ update_employee_excel.py
@@ -0,0 +1,44 @@
+import pandas as pd
+import subprocess
+from ldap3 import Server, Connection, SAFE_SYNC, ALL_ATTRIBUTES, SUBTREE, BASE, ALL
+from getpass import getpass
+import re
+
+#Pass excel sheet into a dataframe
+df = pd.read_excel('TCMD LIst.xls', keep_default_na=False)
+#password = getpass()
+
+#Loops over each row in the dataframe
+for index, row in df.iterrows():
+    employeeLName = row["Last Name"] 
+    employeeUsrName = row["Network Username"]
+    employeeEmail = row["mail"]
+    employeeDepartment = row["department"]
+    #print(employeeUsrName)
+
+    try:
+        proc =subprocess.check_output(["C:\\WINDOWS\\system32\\WindowsPowerShell\\v1.0\\powershell.exe", f"Get-AdUser '{employeeUsrName}' -Properties Department, Mail, Surname | fl Surname, SamAccountName, Mail, Department"]).decode("utf-8")
+    
+        employeeSurname = re.split(':|\n', proc)[3]
+        employeeMail = re.split(':|\n', proc)[7]
+        employeeDepart = re.split(':|\n', proc)[9]
+        
+        print(row)
+        df.at[index, "Last Name"]=employeeSurname
+        df.at[index, "mail"]=employeeMail
+        df.at[index, "department"]=employeeDepart
+    except Exception as e:
+        message = str(e)
+
+
+df.to_excel("output.xlsx")
+
+
+    
+#server = Server('172.16.0.6', get_info=ALL)
+#conn = Connection(server, 'nib-bahamas\z-devaughn', password, auto_bind=True)
+#conn.open()
+#conn.search("DC=nib-bahamas,DC=COM", "(givenName=Devaughn)")
+#conn.entries
+#print(conn.entries)
+
